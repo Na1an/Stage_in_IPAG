405870cce71030c8833b50e32fcda49710cd253d
Yuchen <yuchenbai@hotmail.com>
2021-04-12 01:53:46 +0200

    works! we can collecte the frames of all reference cubes in the SPHERE_DC_DATA, and only select the scale in center

--- RDI.py
+++ RDI.py
@@ -40,6 +40,21 @@ def display_rotaion_angles(data):
     for i in range(len(data)):
         print(str(i)+"th rotation =", data[i])
 
+# slice frame, we only take the interesting area
+# for exemple, 1/4 in the center of each frame
+def slice_frame(frames, size, center_scale):
+    '''
+    Args:
+        frames : np.array, 3 dims. Contains all the frames on one wavelength in a cube
+        size : a int. Frames sizei, size = 1024 in case 1024*1024.
+        center_scale : a float. The scale in center that we will process.
+    Return:
+        Sliced frames, np.array, 3 dims.
+    '''
+    tmp = (1-center_scale)*0.5
+    res = frames[:, int(size*tmp):int(size*(1-tmp)), size*tmp:size*(1-tmp)] 
+    return res
+
 # 1. travesal the SPHERE_DC_DATA and get all the reference master cubes
 def get_reference_cubes(repository_path, keyword):
     '''
@@ -63,24 +78,35 @@ def get_reference_cubes(repository_path, keyword):
     return res
 
 # 2. Collect the data from SPHERE_DC_DATA
-def collect_data(files_path):
+def collect_data(files_path, scale=0.25):
     '''
     Args:
         files_path : a list of string. files path contain keyword.
+        scale : a float. The scale in center that we want process, is equal to 1/4 by default.
     Rrturn:
         res : a list of float. Return ...
     '''
     hd = fits.getdata(files_path[0])
-    '''
-    for f in range(1,len(files_path)):
+    # frames in the first wavelength and second wavelength
+    # K1/K2, H2/H3, etc...
+    tmp = (1-scale)*0.5
+    size = len(hd[0][0])
+    start = int(size*tmp)
+    end = int(size*(1-tmp))
+    
+    frames_fir_wl = hd[0][..., start:end, start:end]
+    frames_sec_wl = hd[1][..., start:end, start:end]
+
+    #print(">> len(first) = ", len(frames_fir_wl))
+
+    for i in range(1,len(files_path)):
         #print(f)
-        hd.append(fits.open(f))
-        hd.info()
-    '''
-    print(">> type hd =", type(hd), " shape", hd.shape)
-    print(">> len(hd) = ", len(hd))
-    res = []
-    return res 
+        hd = fits.getdata(files_path[i])
+        frames_fir_wl = np.append(frames_fir_wl, hd[0][..., start:end, start:end], axis=0)
+        frames_sec_wl = np.append(frames_sec_wl, hd[1][..., start:end, start:end], axis=0)
+        #print(">> len(first) = ", len(frames_fir_wl))
+    
+    return frames_fir_wl, frames_sec_wl
 
 # main bloc
 if __name__ == "__main__":
@@ -105,7 +131,15 @@ if __name__ == "__main__":
     all_files = get_reference_cubes(str(sys.argv[2]), "MASTER_CUBE-center")
    
     # Step 2: put the related data (all frames of the reference cubes) in np.array
-    reference_frames = collect_data(all_files) 
-
+    ref_frames_wl1, ref_frames_wl2 = collect_data(all_files)
+    print(ref_frames_wl1.shape, "test value luminosity =", ref_frames_wl1[50][121][122])
+    print(ref_frames_wl2.shape, "test value luminosity =", ref_frames_wl2[50][121][122])
+    
+    #plt.style.use('seaborn-white')
+    plt.subplot(1,2,1)
+    plt.imshow(ref_frames_wl1[50], cmap=plt.cm.hot)
+    plt.subplot(1,2,2)
+    plt.imshow(ref_frames_wl2[50], cmap=plt.cm.hot)
+    plt.show()
     start_and_end(False)
 
