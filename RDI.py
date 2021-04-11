d7b361af7c8e1ce1c26238cf64c15f8cc24a4fa4
Na1an <yuchenbai@hotmail.com>
2021-04-11 22:25:56 +0200

    process reference cubes

--- RDI.py
+++ RDI.py
@@ -5,6 +5,13 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from matplotlib.widgets import Slider
 
+# program start and program end 
+def start_and_end(start):
+    if(start):
+        print("######### program start ###########\n")
+    else:
+        print("\n########## program end ############")
+
 # read one file and return its data
 def read_file(file_path):
     '''
@@ -16,7 +23,10 @@ def read_file(file_path):
     hd = fits.open(file_path)
     # print("filename =", hd.fileinfo(0)["filename"].split("/")[-1])
     hd.info()
-    return hd[0].data
+    print('\n')
+    data = hd[0].data
+    hd.close()
+    return data
 
 # display the list of rotation angles
 def display_rotaion_angles(data):
@@ -30,7 +40,7 @@ def display_rotaion_angles(data):
     for i in range(len(data)):
         print(str(i)+"th rotation =", data[i])
 
-# travesal the SPHERE_DC_DATA and get all the reference master cubes
+# 1. travesal the SPHERE_DC_DATA and get all the reference master cubes
 def get_reference_cubes(repository_path, keyword):
     '''
     Args:
@@ -52,6 +62,26 @@ def get_reference_cubes(repository_path, keyword):
 
     return res
 
+# 2. Collect the data from SPHERE_DC_DATA
+def collect_data(files_path):
+    '''
+    Args:
+        files_path : a list of string. files path contain keyword.
+    Rrturn:
+        res : a list of float. Return ...
+    '''
+    hd = fits.getdata(files_path[0])
+    '''
+    for f in range(1,len(files_path)):
+        #print(f)
+        hd.append(fits.open(f))
+        hd.info()
+    '''
+    print(">> type hd =", type(hd), " shape", hd.shape)
+    print(">> len(hd) = ", len(hd))
+    res = []
+    return res 
+
 # main bloc
 if __name__ == "__main__":
     '''
@@ -64,11 +94,18 @@ if __name__ == "__main__":
     data = read_file(str(sys.argv[1]))
     display_rotaion_angles(data)
     '''
+    
+    start_and_end(True)
+    
     # argv1 : science object
     science_frames = read_file(str(sys.argv[1]))
-    print(type(science_frames))
+    print(">> Science frames type", type(science_frames),'\n')
 
-    # argv2 : the SPHERE_DC/repository path
+    # Step 1: get the list of files contain keyword
     all_files = get_reference_cubes(str(sys.argv[2]), "MASTER_CUBE-center")
-    for f in all_files:
-        print(f)
+   
+    # Step 2: put the related data (all frames of the reference cubes) in np.array
+    reference_frames = collect_data(all_files) 
+
+    start_and_end(False)
+
