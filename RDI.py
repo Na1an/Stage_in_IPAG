741a8bdbf62a477f4181ce8449104ac47615d74d
Yuchen <yuchenbai@hotmail.com>
2021-04-12 13:18:59 +0200

    severals methodes

--- RDI.py
+++ RDI.py
@@ -4,6 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 from matplotlib.widgets import Slider
+from sklearn.metrics import log_loss
+from scipy import stats
+from skimage.metrics import structural_similarity as ssim
 
 # program start and program end 
 def start_and_end(start):
@@ -105,6 +108,7 @@ def collect_data(files_path, scale=0.25):
     return ref_frames
 
 # 3. process the science frames, substract the starlight
+# wavelength to wavelength
 def process_RDI(science_frames, ref_frames):
     '''
     Args:
@@ -136,9 +140,85 @@ def process_RDI(science_frames, ref_frames):
                     tmp = pearson_corr
                     indice = j
             res[wl, i] = science_frames[wl, i] - ref_frames[wl, indice]
+
             print("---", i+1, "of", sc_fr_nb,"---")
         print("<< wavelength = ", wl, "end process")
     return res
+
+# frames to frames
+def process_RDI_bis(science_frames, ref_frames):
+    '''
+    Args:
+        science_frames : a numpy.ndarray. (wavelengths, nb_frames, x, y)
+        ref_frames : a numpy.ndarray. (wavelengths, nb_frames, x, y)
+        Normally, the scale in 4 dimens is consistent for two args.
+    Return:
+        don't know yet.
+    '''
+    print(">> science_frame shap =", science_frames.shape) 
+    print(">> ref_frame shap =", ref_frames.shape)
+   
+    wave_length = len(science_frames)
+    sc_fr_nb = len(science_frames[0])
+    rf_fr_nb = len(ref_frames[0])
+    side_len = len(science_frames[0,0])
+    
+    res = np.zeros((wave_length, sc_fr_nb, side_len, side_len))
+    # for both wavelength
+    for wl in range(wave_length):
+        print(">> wavelength =", wl, "start process")
+        for i in range(sc_fr_nb):
+            tmp = -1
+            pearson_corr = -1
+            indice = 0 #most relevent indicfor j in range(ref_frames):
+            for j in range(rf_fr_nb):
+                for wl2 in range(wave_length):
+                    pearson_corr = np.corrcoef(science_frames[wl, i], ref_frames[wl2, j])[0,1]
+                    if(pearson_corr>tmp):
+                        tmp = pearson_corr
+                        indice = j
+            res[wl, i] = science_frames[wl, i] - ref_frames[wl, indice]
+
+            print("---", i+1, "of", sc_fr_nb,"---")
+        print("<< wavelength = ", wl, "end process")
+    return res
+
+def process_RDI_ssim(science_frames, ref_frames):
+    '''
+    Args:
+        science_frames : a numpy.ndarray. (wavelengths, nb_frames, x, y)
+        ref_frames : a numpy.ndarray. (wavelengths, nb_frames, x, y)
+        Normally, the scale in 4 dimens is consistent for two args.
+    Return:
+        don't know yet.
+    '''
+    print(">> science_frame shap =", science_frames.shape) 
+    print(">> ref_frame shap =", ref_frames.shape)
+   
+    wave_length = len(science_frames)
+    sc_fr_nb = len(science_frames[0])
+    rf_fr_nb = len(ref_frames[0])
+    side_len = len(science_frames[0,0])
+    
+    res = np.zeros((wave_length, sc_fr_nb, side_len, side_len))
+    # for both wavelength
+    for wl in range(wave_length):
+        print(">> wavelength =", wl, "start process")
+        for i in range(sc_fr_nb):
+            tmp = -1
+            ssim_value = -1
+            indice = 0 #most relevent indicfor j in range(ref_frames):
+            for j in range(rf_fr_nb):
+                ssim_value = ssim(science_frames[wl, i], ref_frames[wl, j])
+                if(ssim_value>tmp):
+                    tmp = ssim_value
+                    indice = j
+            res[wl, i] = science_frames[wl, i] - ref_frames[wl, indice]
+
+            print("---", i+1, "of", sc_fr_nb,"---")
+        print("<< wavelength = ", wl, "end process")
+    return res
+
 
 # main bloc
 if __name__ == "__main__":
@@ -169,13 +249,20 @@ if __name__ == "__main__":
     #print(ref_frames[1].shape, "test value luminosity =", ref_frames[1, 50, 121, 122])
     
     # Step 3: process the science frames
-    sc_frames_procced = process_RDI(slice_frame(science_frames, len(science_frames[0][0][0]), 0.25), ref_frames)
-    plt.subplot(1,2,1)
+    sc_frames_procced = process_RDI_ssim(slice_frame(science_frames, len(science_frames[0][0][0]), 0.25), ref_frames)
+    plt.subplot(2,3,1)
     plt.imshow(sc_frames_procced[0][0], cmap=plt.cm.hot)
-    plt.subplot(1,2,2)
+    plt.subplot(2,3,2)
+    plt.imshow(sc_frames_procced[0][1], cmap=plt.cm.hot)
+    plt.subplot(2,3,3)
+    plt.imshow(sc_frames_procced[0][2], cmap=plt.cm.hot)
+    plt.subplot(2,3,4)
     plt.imshow(sc_frames_procced[1][0], cmap=plt.cm.hot)
+    plt.subplot(2,3,5)
+    plt.imshow(sc_frames_procced[1][1], cmap=plt.cm.hot)
+    plt.subplot(2,3,6)
+    plt.imshow(sc_frames_procced[1][2], cmap=plt.cm.hot)
     plt.show()
-
 
     # Step 4: comparaison
     '''
