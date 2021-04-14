38fe9da039749e7c7194a62a2e25d7362336ee2f
Yuchen <yuchenbai@hotmail.com>
2021-04-14 20:48:31 +0200

    colorbar is good

--- RDI.py
+++ RDI.py
@@ -166,18 +166,20 @@ def process_ADI(science_frames, rotations):
     '''
     axs[0, 0].imshow(f_median[0], cmap=plt.cm.seismic)
     axs[0, 0].set_title("median wave length 1")
-    axs[0, 1].imshow(f_median[1], cmap=plt.cm.seismic)
+    im2 = axs[0, 1].imshow(f_median[1], cmap=plt.cm.seismic)
     axs[0, 1].set_title("median wave length 2")
     axs[1, 0].imshow(res[0], cmap=plt.cm.seismic)
     axs[1, 0].set_title("res wave length 1")
-    im3 = axs[1, 1].imshow(res[1], cmap=plt.cm.seismic)
+    im4 = axs[1, 1].imshow(res[1], cmap=plt.cm.seismic)
     axs[1, 1].set_title("res wave length 2")
     
-    fig.subplots_adjust(right=0.9)
-    position = fig.add_axes([0.92, 0.12, 0.015, .78 ])#位置[左,下,右,上]
-    cb = fig.colorbar(im3, cax=position)
+    #fig.subplots_adjust(right=1.9)
+    #pos1 = fig.add_axes([0.92, 0.41, 0.015, 0.77])#位置[左,下,右,上]
+    cb = fig.colorbar(im2, ax=axs[0,1])
+    
+    #pos2 = fig.add_axes([0.92, 0.11, 0.015, 0.30])#位置[左,下,右,上]
+    cb = fig.colorbar(im4, ax=axs[1,1])
 
-    #fig.colorbar(pcm1, ax=axs[0], fraction=0.046, pad=0.04)
     plt.show()
     
     return None 
