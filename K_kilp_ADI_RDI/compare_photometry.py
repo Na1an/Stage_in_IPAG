d7f5862c43e54b03b0159571ea4efbc752080149
Na1an <yuchenbai@hotmail.com>
2021-04-27 13:01:12 +0200

    fix bug

--- K_kilp_ADI_RDI/compare_photometry.py
+++ K_kilp_ADI_RDI/compare_photometry.py
@@ -2,7 +2,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
-arcsinhimport matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 from astropy.io import fits
 from photutils.aperture import CircularAperture, aperture_photometry, CircularAnnulus
