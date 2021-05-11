1f4c8c1a7d304e35b534aa36e321a2dfeee42c74
Na1an <yuchenbai@hotmail.com>
2021-05-11 15:18:22 +0200

    modifier compare

--- K_kilp_ADI_RDI/compare_photometry.py
+++ K_kilp_ADI_RDI/compare_photometry.py
@@ -9,14 +9,18 @@ from astropy.io import fits
 from astropy.visualization import simple_norm
 from photutils.aperture import CircularAperture, aperture_photometry, CircularAnnulus
 
-start_time = datetime.datetime.now() 
+# if show the aperture and the annulus of companion's position
 SHOW_POSITION = False
 
-positions = [(126.05284, 249.11)]
-aperture = CircularAperture(positions, r=2)
-annulus = CircularAnnulus(positions, r_in=4, r_out=6)
-
+# Calculate the stdev of the annulus where the companion is located.
 def get_stdev(data):
+    '''
+    Args:
+        data : a np.array, 2 dimensions. A image.
+    Rrturn:
+        np.std(pixels) : a float. Return the stdev of the annulus where the companion is located.
+    ''' 
+    
     radius = int((128-125.99284)**2+(128-248.92338)**2)
     pixels = []
     for i in range(256):
@@ -27,7 +31,16 @@ def get_stdev(data):
                 pixels.append(data[i][j])
     return np.std(pixels)
 
+# Return the 
 def get_photometry(path):
+    '''
+    Args:
+        path : a string. The path of repository where the files are.
+    Rrturn:
+        res : a np.array, 1 dimension. Store the list of each companion's flux.
+        SN : a np.array, 1 dimension. Store the list of each companion's Signal to Noise ratio.
+    '''
+    
     files = os.listdir(path)
     files.sort()
     res = np.zeros((len(files)))
@@ -42,7 +55,6 @@ def get_photometry(path):
         bkg_sum_in_companion = bkg_mean * aperture.area 
         annulus_stdev = get_stdev(data)
         res[i] = flux_companion['aperture_sum_0'] - bkg_sum_in_companion
-        #res[i] = flux_companion['aperture_sum_0']
         SN[i] = res[i] / (annulus_stdev * aperture.area)
         if i==1 and SHOW_POSITION:
             norm = simple_norm(data, 'sqrt', percent=99)
@@ -58,101 +70,109 @@ def get_photometry(path):
 
     return res, SN 
 
-# ADI data
-#ADI_res, ADI_SN = get_photometry("./ADI")
-#ADI_res, ADI_SN = get_photometry("./ADI_WITH_MASK")
-#ADI_res_32, ADI_SN_32 = get_photometry("./ADI_WITH_MASK_32")
-#print(ADI_res_32)
+if __name__ == "__main__":
+    
+    print("###### Start to process the data ######")
+    start_time = datetime.datetime.now() 
+    positions = [(126.05284, 249.11)]
+    aperture = CircularAperture(positions, r=2)
+    annulus = CircularAnnulus(positions, r_in=4, r_out=6)
 
-# RDI data 1 target 2 ref stars
-#RDI_res_2_ref, RDI_2_SN = get_photometry("./RDI_ref_2_star")
-#print(RDI_res_2_ref)
+    # ADI data
+    #ADI_res, ADI_SN = get_photometry("./ADI")
+    #ADI_res, ADI_SN = get_photometry("./ADI_WITH_MASK")
+    #ADI_res_32, ADI_SN_32 = get_photometry("./ADI_WITH_MASK_32")
+    #print(ADI_res_32)
 
-# RDI data 1 target 4 ref stars
-#RDI_res_4_ref, RDI_4_SN = get_photometry("./RDI_ref_4_star")
-#print(RDI_res_4_ref)
+    # RDI data 1 target 2 ref stars
+    #RDI_res_2_ref, RDI_2_SN = get_photometry("./RDI_ref_2_star")
+    #print(RDI_res_2_ref)
 
-#
-#RDI_flux_3_best, RDI_SN_3_best = get_photometry("./RDI_After_3_best")
-#RDI_flux_5_best, RDI_SN_5_best = get_photometry("./RDI_WITH_MASK_5_best")
+    # RDI data 1 target 4 ref stars
+    #RDI_res_4_ref, RDI_4_SN = get_photometry("./RDI_ref_4_star")
+    #print(RDI_res_4_ref)
 
-RDI_flux_3_best_32, RDI_SN_3_best_32 = get_photometry("./RDI_WITH_MASK_3_best_32")
-RDI_flux_5_best_32, RDI_SN_5_best_32 = get_photometry("./RDI_WITH_MASK_5_best_32")
-RDI_flux_7_best, RDI_SN_7_best = get_photometry("./RDI_WITH_MASK_7_best_32")
+    #
+    #RDI_flux_3_best, RDI_SN_3_best = get_photometry("./RDI_After_3_best")
+    #RDI_flux_5_best, RDI_SN_5_best = get_photometry("./RDI_WITH_MASK_5_best")
 
+    RDI_flux_3_best_32, RDI_SN_3_best_32 = get_photometry("./RDI_WITH_MASK_3_best_32")
+    RDI_flux_5_best_32, RDI_SN_5_best_32 = get_photometry("./RDI_WITH_MASK_5_best_32")
+    RDI_flux_7_best, RDI_SN_7_best = get_photometry("./RDI_WITH_MASK_7_best_32")
 
-sns.set(style="darkgrid")
+    sns.set(style="darkgrid")
 
-# nb of data
-nb_data = 3
-data = np.zeros((len(RDI_flux_3_best_32), nb_data))
-for i in range(len(RDI_flux_3_best_32)):
-    '''
-    data[i][0] = 65.20625576328908 
-    data[i][1] = RDI_flux_3_best[i]
-    data[i][2] = RDI_flux_5_best[i]
-    data[i][3] = ADI_res[i]
-    data[i][4] = RDI_flux_3_best_32[i]
-    data[i][5] = RDI_flux_5_best_32[i]
-    data[i][6] = ADI_res_32[i] 
-    '''
-    data[i][0] = RDI_flux_7_best[i]
-    data[i][1] = RDI_flux_3_best_32[i]
-    data[i][2] = RDI_flux_5_best_32[i]
-    '''
-    data[i][0] = ADI_res[i]
-    data[i][1] = ADI_res_32[i] 
-    data[i][2] = RDI_flux_3_best[i]
-    data[i][3] = RDI_flux_3_best_32[i]
-    '''
-data_SN = np.zeros((len(RDI_SN_3_best_32), nb_data))
-for i in range(len(RDI_SN_3_best_32)):
-    '''
-    data_SN[i][0] = ADI_SN[i]
-    data_SN[i][1] = RDI_SN_3_best[i]
-    data_SN[i][2] = RDI_SN_5_best[i]
-    data_SN[i][3] = ADI_SN_32[i]
-    data_SN[i][4] = RDI_SN_3_best_32[i]
-    data_SN[i][5] = RDI_SN_5_best_32[i]
-    data_SN[i][6] = 0
-    '''
-    data_SN[i][2] = RDI_SN_7_best[i]
-    data_SN[i][0] = RDI_SN_3_best_32[i]
-    data_SN[i][1] = RDI_SN_5_best_32[i]
+    # nb of data
+    nb_data = 3
+    data = np.zeros((len(RDI_flux_3_best_32), nb_data))
+    for i in range(len(RDI_flux_3_best_32)):
+        '''
+        data[i][0] = 65.20625576328908 
+        data[i][1] = RDI_flux_3_best[i]
+        data[i][2] = RDI_flux_5_best[i]
+        data[i][3] = ADI_res[i]
+        data[i][4] = RDI_flux_3_best_32[i]
+        data[i][5] = RDI_flux_5_best_32[i]
+        data[i][6] = ADI_res_32[i] 
+        '''
+        data[i][0] = RDI_flux_7_best[i]
+        data[i][1] = RDI_flux_3_best_32[i]
+        data[i][2] = RDI_flux_5_best_32[i]
+        '''
+        data[i][0] = ADI_res[i]
+        data[i][1] = ADI_res_32[i] 
+        data[i][2] = RDI_flux_3_best[i]
+        data[i][3] = RDI_flux_3_best_32[i]
+        '''
+    data_SN = np.zeros((len(RDI_SN_3_best_32), nb_data))
+    for i in range(len(RDI_SN_3_best_32)):
+        '''
+        data_SN[i][0] = ADI_SN[i]
+        data_SN[i][1] = RDI_SN_3_best[i]
+        data_SN[i][2] = RDI_SN_5_best[i]
+        data_SN[i][3] = ADI_SN_32[i]
+        data_SN[i][4] = RDI_SN_3_best_32[i]
+        data_SN[i][5] = RDI_SN_5_best_32[i]
+        data_SN[i][6] = 0
+        '''
+        data_SN[i][2] = RDI_SN_7_best[i]
+        data_SN[i][0] = RDI_SN_3_best_32[i]
+        data_SN[i][1] = RDI_SN_5_best_32[i]
 
+        '''
+        data_SN[i][0] = ADI_SN[i]
+        data_SN[i][1] = ADI_SN_32[i] 
+        data_SN[i][2] = RDI_SN_3_best[i]
+        data_SN[i][3] = RDI_SN_3_best_32[i]
+        '''
     '''
-    data_SN[i][0] = ADI_SN[i]
-    data_SN[i][1] = ADI_SN_32[i] 
-    data_SN[i][2] = RDI_SN_3_best[i]
-    data_SN[i][3] = RDI_SN_3_best_32[i]
+    data_total = pd.DataFrame(data[:,:], columns=['ADI_Full_Frame_3_best','ADI_Outer_Region_3_best','RDI_Full_Frame_3_best','RDI_Outer_Region_3_best'])
+    #data_total = pd.DataFrame(data[:,:], columns=['ADI_Outer_Region_3_best','RDI_Outer_Region_3_best'])
+    data_total.index = data_total.index + 1
+    print("######### Flux of companion #######")
+    print(data_total)
+    data_total.to_csv("Flux_of_companion.csv")
+    #data_total.plot(kind='line', style='--o', title='comparation')
+    sns.relplot(kind='line',data=data_total)
+    #plt.title("Target:GJ667c Ref: 9 others stars")
+    plt.xlabel("K_kilp")
+    plt.ylabel("Flux of the companion absolute - diameter 4 px")
+    #plt.ylim(0,70)
+    plt.show()
     '''
-'''
-data_total = pd.DataFrame(data[:,:], columns=['ADI_Full_Frame_3_best','ADI_Outer_Region_3_best','RDI_Full_Frame_3_best','RDI_Outer_Region_3_best'])
-#data_total = pd.DataFrame(data[:,:], columns=['ADI_Outer_Region_3_best','RDI_Outer_Region_3_best'])
-data_total.index = data_total.index + 1
-print("######### Flux of companion #######")
-print(data_total)
-data_total.to_csv("Flux_of_companion.csv")
-#data_total.plot(kind='line', style='--o', title='comparation')
-sns.relplot(kind='line',data=data_total)
-#plt.title("Target:GJ667c Ref: 9 others stars")
-plt.xlabel("K_kilp")
-plt.ylabel("Flux of the companion absolute - diameter 4 px")
-#plt.ylim(0,70)
-plt.show()
-'''
-data_total_SN = pd.DataFrame(data_SN[:,:], columns=['RDI_3_best_Outer_Region','RDI_5_best_Outer_Region','RDI_7_best_Outer_Region'])
-#data_total_SN = pd.DataFrame(data_SN[:,:], columns=['S/N_ADI_Outer_Region_3_best','S/N_RDI_Outer_Region_3_best'])
-data_total_SN.index = data_total_SN.index + 1
-print("######### S/N ########")
-print(data_total_SN)
-#data_total_SN.to_csv("SN_companions.csv")
-#data_total.plot(kind='line', style='--o', title='comparation')
-ax = sns.relplot(kind='line',data=data_total_SN)
-plt.legend(fontsize = '16')
-#plt.title("Target:GJ667c Ref: 9 other stars")
-plt.xlabel("K_kilp", fontsize= "16")
-plt.ylabel("S/N - diameter 4 px", fontsize = "16")
-plt.show()
-end_time = datetime.datetime.now() 
-print("cost :", end_time - start_time)
+    data_total_SN = pd.DataFrame(data_SN[:,:], columns=['RDI_3_best_Outer_Region','RDI_5_best_Outer_Region','RDI_7_best_Outer_Region'])
+    #data_total_SN = pd.DataFrame(data_SN[:,:], columns=['S/N_ADI_Outer_Region_3_best','S/N_RDI_Outer_Region_3_best'])
+    data_total_SN.index = data_total_SN.index + 1
+    print("######### S/N ########")
+    print(data_total_SN)
+    #data_total_SN.to_csv("SN_companions.csv")
+    #data_total.plot(kind='line', style='--o', title='comparation')
+    ax = sns.relplot(kind='line',data=data_total_SN)
+    plt.legend(fontsize = '16')
+    #plt.title("Target:GJ667c Ref: 9 other stars")
+    plt.xlabel("K_kilp", fontsize= "16")
+    plt.ylabel("S/N - diameter 4 px", fontsize = "16")
+    plt.show()
+    end_time = datetime.datetime.now() 
+    print("cost :", end_time - start_time)
+    print("###### End of program ######")
