753fa1633788a65e4a0bf9e39a200b20600d66c4
Na1an <yuchenbai@hotmail.com>
2021-06-08 15:10:54 +0200

    clean code

--- main.py
+++ main.py
@@ -162,6 +162,187 @@ def selection(nb_best, target, refs, scale, wave_length=0):
 def rdi_detection():
     return None
 
+# option for main : RDI
+def RDI(argv, scale):
+    print(">> Algo PCA is working! ")
+    start_time = datetime.datetime.now()
+    if(len(argv) >4):
+        scale = float(argv[4])
+
+    # 1. get target
+    target_path = str(argv[2])
+    science_target = read_file(target_path, "fake_comp_added_disk")
+    science_target_croped = crop_frame(science_target, len(science_target[0,0,0]), scale)
+    print("Scale =", scale, "\n science target shape =", science_target_croped.shape)
+    
+    # 2. get the list of files in library
+    ref_files = get_reference_cubes(str(argv[3]), "MASTER_CUBE-center")
+    
+    # Check if the taget is in the ref files, if true remove it
+    ref_files = remove_target(str(argv[2]),ref_files)
+    print(">> what we have in ref_res")
+    for s in ref_files:
+        print(s)
+
+    # Select the best correlated targets
+    # count is the number of we want to chose
+    count = int(argv[5])
+    ref_files = selection(count, science_target_croped, ref_files, scale, 0) # 0 is the default wave length
+
+    # 3. put the related data (all frames of the reference cubes) in np.array
+    ref_frames = collect_data(ref_files, scale)
+    print("ref_frames shape =", ref_frames.shape)
+    
+    # get angles
+    angles = read_file(str(argv[2]), "ROTATION")
+    
+    # get ref shape
+    wl_ref, nb_fr_ref, w, h = ref_frames.shape
+    wl = 1
+    n = nb_fr_ref
+    
+    # create outer mask
+    r_in = 15
+    r_out = 125
+    outer_mask, n_pxls = create_outer_mask(w,h,r_out)
+    science_target_croped[wl] = science_target_croped[wl]*outer_mask
+    
+    for i in range(1,51):
+        res_tmp = vip.pca.pca_fullfr.pca(science_target_croped[wl], -angles, ncomp= i, mask_center_px=r_in, cube_ref=ref_frames[wl]*outer_mask, scaling='spat-mean')
+        #res_tmp = vip.pca.pca_local.pca_annular(science_target_croped[wl], -angles, cube_ref=ref_frames[wl], radius_int=r_in, asize=96, ncomp=i, scaling='spat-mean')
+        path = "./K_kilp_ADI_RDI/disk/"+"{0:05d}".format(i) + ".fits"            
+        hdu = fits.PrimaryHDU(res_tmp)
+        hdu.writeto(path)
+        print(">>===", i, "of", n,"=== fits writed to === path:", path)
+    end_time = datetime.datetime.now()
+    print("PCA on RDI ", n," take", end_time - start_time)
+
+# option for main : ADI
+def ADI(argv, scale):
+    print(">> Algo PCA ADI is working! ")
+    start_time = datetime.datetime.now()
+    if(len(sys.argv) >4):
+        scale = float(sys.argv[4])
+
+    # 1. get target
+    target_path = str(sys.argv[2])
+    science_target = read_file(target_path, "fake")
+    science_target_croped = crop_frame(science_target, len(science_target[0,0,0]), scale)
+    print("Scale =", scale, "\n science target shape =", science_target_croped.shape)
+    
+    # get angles
+    angles = read_file(str(sys.argv[2]), "ROTATION")
+    
+    # get science target shape
+    wl_ref, nb_fr_ref, w, h = science_target_croped.shape
+    wl = 0
+    n = nb_fr_ref
+    
+    # create outer mask
+    r_in = 15 
+    r_out = 125
+    outer_mask, n_pxls = create_outer_mask(w,h,r_out)
+    science_target_croped[wl] = science_target_croped[wl]*outer_mask
+
+    for i in range(1,n+1):
+        #res_tmp = vip.pca.pca_fullfr.pca(science_target_croped[wl], -angles, ncomp= i, mask_center_px=MASK_RADIUS, cube_ref=ref_frames[wl], scaling='temp-mean')
+        #res_tmp = vip.pca.pca_local.pca_annular(science_target_croped[wl], -angles, radius_int=118, asize=7, ncomp=i)
+        #res_tmp = vip.pca.pca_annular(science_target_croped[wl], -angles, cube_ref=science_target_croped[wl],radius_int=118, asize=7, ncomp=i, scaling='temp-mean')
+        res_tmp = vip.pca.pca_fullfr.pca(science_target_croped[wl], -angles, ncomp= i, mask_center_px=r_in, scaling='temp-mean')
+
+        #path = "./K_kilp_ADI_RDI/Test_ADI/ADI_Masked" + "{0:05d}".format(i) + ".fits"
+        path = "./K_kilp_ADI_RDI/fake_planet/ADI_Masked" + "{0:05d}".format(i) + ".fits"
+        hdu = fits.PrimaryHDU(res_tmp)
+        hdu.writeto(path)
+        print(">>===", i, "of", n,"=== fits writed ===")
+
+    end_time = datetime.datetime.now()
+    
+    print("PCA on ADI ", n," take", end_time - start_time)
+
+# option for main : INJECTION
+def INJECTION(argv, scale):
+    print(">> Inject a fake companion or a disk")
+        
+    # 1. get target
+    target_path = str(argv[2])
+    science_target = read_file(target_path, "MASTER_CUBE-center")
+    obj = "abc"
+    if(len(argv) >4):
+        scale = float(argv[4])
+    if(len(argv) >5):
+        obj = str(argv[5]).upper()
+        print("obj =", obj)
+    print("science target shape =", science_target.shape)
+    
+    # 2. prepare these parameters
+    # get angles
+    angles = read_file(target_path, "ROTATION")
+    psf = read_file(target_path, "PSF_MASTER_CUBE") 
+    
+    # get science target shape
+    wl_ref, nb_fr_ref, w, h = science_target.shape
+    wl = 0
+    
+    # fwhm psfn
+    fwhm = get_fwhm_from_psf(psf[wl])
+    psfn = vip.metrics.normalize_psf(psf[wl], fwhm, size=17)
+    print("psfn =", psfn.shape, "psfn.ndim =", psfn.ndim)
+
+    # pxscale of IRDIS
+    pxscale = get_pxscale()
+
+    # make fake companion
+    if obj == "PLANETE":
+        fake_comp_0 = vip.metrics.cube_inject_companions(science_target[wl], psf_template=psfn, angle_list=-angles, flevel=40, plsc=pxscale, rad_dists=[40], theta=160, n_branches = 1)
+        fake_comp_1 = vip.metrics.cube_inject_companions(science_target[1], psf_template=psfn, angle_list=-angles, flevel= 1000, plsc=pxscale, rad_dists=[40], theta=160, n_branches = 1)
+        print("fake companion 0 shape = ", fake_comp_0.shape)
+        fake_comp = np.zeros((wl_ref, nb_fr_ref, w, h))
+        fake_comp[0] = fake_comp_0
+        fake_comp[1] = fake_comp_1
+        path_fake_comp = "./K_kilp_ADI_RDI/fake_planet/fake_comp02.fits"
+
+        hdu = fits.PrimaryHDU(fake_comp)
+        hdu.writeto(path_fake_comp) 
+    
+    elif obj == "DISK":
+        dstar = 35 # distance to the star in pc, the bigger the disk if more small and more close to star
+        nx = 256 # number of pixels of your image in X
+        ny = 256 # number of pixels of your image in Y
+        itilt = 65 # inclination of your disk in degreess (0 means pole-on -> can see the full plate, 90 means edge on -> only see a line)
+        pa = -50 # position angle of the disk in degrees (0 means north, 90 means east)
+        a = 40 # semimajoraxis of the disk in au / semimajor axis in arcsec is 80 au/80px = 1 arcsec
+        fake_disk1 = vip.metrics.scattered_light_disk.ScatteredLightDisk(\
+                    nx=nx,ny=ny,distance=dstar,\
+                    itilt=itilt,omega=0,pxInArcsec=pxscale,pa=pa,\
+                    density_dico={'name':'2PowerLaws','ain':4,'aout':-4,\
+                    'a':a,'e':0.0,'ksi0':1.,'gamma':2.,'beta':1.},\
+                    spf_dico={'name':'HG', 'g':0., 'polar':False})
+        fake_disk1_map = fake_disk1.compute_scattered_light()
+        fake_disk1_map = fake_disk1_map/np.max(fake_disk1_map)
+        #ds9 = vip.Ds9Window()
+        #ds9.display(fake_disk1_map)
+        
+        # add fake disk to science target
+        scaling_factor = 0.1
+        cube_fakeddisk = vip.metrics.cube_inject_fakedisk(fake_disk1_map*scaling_factor ,angle_list=angles,psf=psfn)
+        
+        # only want center
+        start = int(w*(1-scale)/2)
+        end = int(start+w*scale)
+        science_target[0,:,start:end,start:end] = science_target[0,:,start:end,start:end] + cube_fakeddisk
+        cube_fakeddisk = vip.metrics.cube_inject_fakedisk(fake_disk1_map,angle_list=angles,psf=psfn)
+        science_target[1,:,start:end,start:end] = science_target[1,:,start:end,start:end] + cube_fakeddisk*100
+        path_fake_disk = "./K_kilp_ADI_RDI/fake_planet/"+str(argv[6])
+        hdu = fits.PrimaryHDU(science_target)
+        hdu.writeto(path_fake_disk)
+    else:
+        print("No such object inject option")
+    
+    # display
+    #ds9 = vip.Ds9Window()
+    #ds9.display(fake_comp[0])
+
 # option for main : SCAL
 def SCAL(argv, scale):
     print(">> Analysis scalings effect! ")
@@ -376,187 +557,20 @@ if __name__ == "__main__":
     
     if opt == "RDI":
         # product the processed images    
-        print(">> Algo PCA is working! ")
-        start_time = datetime.datetime.now()
-        if(len(sys.argv) >4):
-            scale = float(sys.argv[4])
-
-        # 1. get target
-        target_path = str(sys.argv[2])
-        science_target = read_file(target_path, "fake_comp_added_disk")
-        science_target_croped = crop_frame(science_target, len(science_target[0,0,0]), scale)
-        print("Scale =", scale, "\n science target shape =", science_target_croped.shape)
-        
-        # 2. get the list of files in library
-        ref_files = get_reference_cubes(str(sys.argv[3]), "MASTER_CUBE-center")
-        
-        # Check if the taget is in the ref files, if true remove it
-        ref_files = remove_target(str(sys.argv[2]),ref_files)
-        print(">> what we have in ref_res")
-        for s in ref_files:
-            print(s)
-
-        # Select the best correlated targets
-        # count is the number of we want to chose
-        count = int(sys.argv[5])
-        ref_files = selection(count, science_target_croped, ref_files, scale, 0) # 0 is the default wave length
-
-        # 3. put the related data (all frames of the reference cubes) in np.array
-        ref_frames = collect_data(ref_files, scale)
-        print("ref_frames shape =", ref_frames.shape)
-        
-        # get angles
-        angles = read_file(str(sys.argv[2]), "ROTATION")
-        
-        # get ref shape
-        wl_ref, nb_fr_ref, w, h = ref_frames.shape
-        wl = 1
-        n = nb_fr_ref
-        
-        # create outer mask
-        r_in = 15
-        r_out = 125
-        outer_mask, n_pxls = create_outer_mask(w,h,r_out)
-        science_target_croped[wl] = science_target_croped[wl]*outer_mask
-        
-        for i in range(1,51):
-            res_tmp = vip.pca.pca_fullfr.pca(science_target_croped[wl], -angles, ncomp= i, mask_center_px=r_in, cube_ref=ref_frames[wl]*outer_mask, scaling='spat-mean')
-            #res_tmp = vip.pca.pca_local.pca_annular(science_target_croped[wl], -angles, cube_ref=ref_frames[wl], radius_int=r_in, asize=96, ncomp=i, scaling='spat-mean')
-            path = "./K_kilp_ADI_RDI/disk/"+"{0:05d}".format(i) + ".fits"            
-            hdu = fits.PrimaryHDU(res_tmp)
-            hdu.writeto(path)
-            print(">>===", i, "of", n,"=== fits writed to === path:", path)
-        end_time = datetime.datetime.now()
-        print("PCA on RDI ", n," take", end_time - start_time)
+        RDI(sys.argv, scale) 
     
     elif opt == "ADI":
         # product the processed images    
-        print(">> Algo PCA ADI is working! ")
-        start_time = datetime.datetime.now()
-        if(len(sys.argv) >4):
-            scale = float(sys.argv[4])
-
-        # 1. get target
-        target_path = str(sys.argv[2])
-        science_target = read_file(target_path, "fake")
-        science_target_croped = crop_frame(science_target, len(science_target[0,0,0]), scale)
-        print("Scale =", scale, "\n science target shape =", science_target_croped.shape)
-        
-        # get angles
-        angles = read_file(str(sys.argv[2]), "ROTATION")
-        
-        # get science target shape
-        wl_ref, nb_fr_ref, w, h = science_target_croped.shape
-        wl = 0
-        n = nb_fr_ref
-        
-        # create outer mask
-        r_in = 15 
-        r_out = 125
-        outer_mask, n_pxls = create_outer_mask(w,h,r_out)
-        science_target_croped[wl] = science_target_croped[wl]*outer_mask
-
-        for i in range(1,n+1):
-            #res_tmp = vip.pca.pca_fullfr.pca(science_target_croped[wl], -angles, ncomp= i, mask_center_px=MASK_RADIUS, cube_ref=ref_frames[wl], scaling='temp-mean')
-            #res_tmp = vip.pca.pca_local.pca_annular(science_target_croped[wl], -angles, radius_int=118, asize=7, ncomp=i)
-            #res_tmp = vip.pca.pca_annular(science_target_croped[wl], -angles, cube_ref=science_target_croped[wl],radius_int=118, asize=7, ncomp=i, scaling='temp-mean')
-            res_tmp = vip.pca.pca_fullfr.pca(science_target_croped[wl], -angles, ncomp= i, mask_center_px=r_in, scaling='temp-mean')
-
-            #path = "./K_kilp_ADI_RDI/Test_ADI/ADI_Masked" + "{0:05d}".format(i) + ".fits"
-            path = "./K_kilp_ADI_RDI/fake_planet/ADI_Masked" + "{0:05d}".format(i) + ".fits"
-            hdu = fits.PrimaryHDU(res_tmp)
-            hdu.writeto(path)
-            print(">>===", i, "of", n,"=== fits writed ===")
-
-        end_time = datetime.datetime.now()
-        
-        print("PCA on ADI ", n," take", end_time - start_time)
+        ADI(sys.argv, scale) 
     
     elif opt == "INJECTION":
-        # inject a fake planet
-        print(">> Inject a fake planet")
-        
-        # 1. get target
-        target_path = str(sys.argv[2])
-        science_target = read_file(target_path, "MASTER_CUBE-center")
-        obj = "abc"
-        if(len(sys.argv) >4):
-            scale = float(sys.argv[4])
-        if(len(sys.argv) >5):
-            obj = str(sys.argv[5]).upper()
-            print("obj =", obj)
-        print("science target shape =", science_target.shape)
-        
-        # 2. prepare these parameters
-        # get angles
-        angles = read_file(target_path, "ROTATION")
-        psf = read_file(target_path, "PSF_MASTER_CUBE") 
-        
-        # get science target shape
-        wl_ref, nb_fr_ref, w, h = science_target.shape
-        wl = 0
-        
-        # fwhm psfn
-        fwhm = get_fwhm_from_psf(psf[wl])
-        psfn = vip.metrics.normalize_psf(psf[wl], fwhm, size=17)
-        print("psfn =", psfn.shape, "psfn.ndim =", psfn.ndim)
+        # inject a fake planet or disk
+        INJECTION(sys.argv, scale)
 
-        # pxscale of IRDIS
-        pxscale = get_pxscale()
-
-        # make fake companion
-        if obj == "PLANETE":
-            fake_comp_0 = vip.metrics.cube_inject_companions(science_target[wl], psf_template=psfn, angle_list=-angles, flevel=40, plsc=pxscale, rad_dists=[40], theta=160, n_branches = 1)
-            fake_comp_1 = vip.metrics.cube_inject_companions(science_target[1], psf_template=psfn, angle_list=-angles, flevel= 1000, plsc=pxscale, rad_dists=[40], theta=160, n_branches = 1)
-            print("fake companion 0 shape = ", fake_comp_0.shape)
-            fake_comp = np.zeros((wl_ref, nb_fr_ref, w, h))
-            fake_comp[0] = fake_comp_0
-            fake_comp[1] = fake_comp_1
-            path_fake_comp = "./K_kilp_ADI_RDI/fake_planet/fake_comp02.fits"
-
-            hdu = fits.PrimaryHDU(fake_comp)
-            hdu.writeto(path_fake_comp) 
-        
-        elif obj == "DISK":
-            dstar = 35 # distance to the star in pc, the bigger the disk if more small and more close to star
-            nx = 256 # number of pixels of your image in X
-            ny = 256 # number of pixels of your image in Y
-            itilt = 65 # inclination of your disk in degreess (0 means pole-on -> can see the full plate, 90 means edge on -> only see a line)
-            pa = -50 # position angle of the disk in degrees (0 means north, 90 means east)
-            a = 40 # semimajoraxis of the disk in au / semimajor axis in arcsec is 80 au/80px = 1 arcsec
-            fake_disk1 = vip.metrics.scattered_light_disk.ScatteredLightDisk(\
-                        nx=nx,ny=ny,distance=dstar,\
-                        itilt=itilt,omega=0,pxInArcsec=pxscale,pa=pa,\
-                        density_dico={'name':'2PowerLaws','ain':4,'aout':-4,\
-                        'a':a,'e':0.0,'ksi0':1.,'gamma':2.,'beta':1.},\
-                        spf_dico={'name':'HG', 'g':0., 'polar':False})
-            fake_disk1_map = fake_disk1.compute_scattered_light()
-            fake_disk1_map = fake_disk1_map/np.max(fake_disk1_map)
-            #ds9 = vip.Ds9Window()
-            #ds9.display(fake_disk1_map)
-            
-            # add fake disk to science target
-            scaling_factor = 0.1
-            cube_fakeddisk = vip.metrics.cube_inject_fakedisk(fake_disk1_map*scaling_factor ,angle_list=angles,psf=psfn)
-            
-            # only want center
-            start = int(w*(1-scale)/2)
-            end = int(start+w*scale)
-            science_target[0,:,start:end,start:end] = science_target[0,:,start:end,start:end] + cube_fakeddisk
-            cube_fakeddisk = vip.metrics.cube_inject_fakedisk(fake_disk1_map,angle_list=angles,psf=psfn)
-            science_target[1,:,start:end,start:end] = science_target[1,:,start:end,start:end] + cube_fakeddisk*100
-            path_fake_disk = "./K_kilp_ADI_RDI/fake_planet/"+str(sys.argv[6])
-            hdu = fits.PrimaryHDU(science_target)
-            hdu.writeto(path_fake_disk)
-        else:
-            print("No such object inject option")
-        
-        # display
-        #ds9 = vip.Ds9Window()
-        #ds9.display(fake_comp[0])
     elif opt == "SCAL":
         # SCAL : analysis scalings effects 
         SCAL(sys.argv, scale)        
+    
     elif opt == "SAM":
         # SAM : spat-annular-mean
         SAM(sys.argv, scale)
